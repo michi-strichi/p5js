a5bb9d22ae8048a9e8f6cf0c63c323e5027eb6fc
Michael-J-Hochreiter <michael.j.hochreiter@gmail.com>
2024-02-17 21:56:55 +0100

    format

--- src/cellular_automata/main.ts
+++ src/cellular_automata/main.ts
@@ -68,7 +68,7 @@ new p5((p: p5) => {
 		}
 	};
 
-	const eraseRadius = (row:number, col:number, radius:number) => {
+	const eraseRadius = (row: number, col: number, radius: number) => {
 		for (let r = -radius; r <= radius; r++) {
 			for (let c = -radius; c <= radius; c++) {
 				const d = p.dist(row, col, row + r, col + c);
@@ -80,76 +80,76 @@ new p5((p: p5) => {
 
 	const updateStates = () => {
 		for (let r = 0; r < ROWS; r++) {
-		  for (let c = 0; c < COLS; c++) {
-			inactiveCells[r][c] = applyRules(r, c);
-		  }
+			for (let c = 0; c < COLS; c++) {
+				inactiveCells[r][c] = applyRules(r, c);
+			}
 		}
-	  
+
 		[activeCells, inactiveCells] = [inactiveCells, activeCells];
-	  };
-	  
-	  const applyRules = (row:number, col:number) => {
+	};
+
+	const applyRules = (row: number, col: number) => {
 		const ownState = activeCells[row][col];
 		return classicRules(row, col, ownState);
-	  };
-	  
-	  const classicRules = (row:number, col:number, ownState:number) => {
+	};
+
+	const classicRules = (row: number, col: number, ownState: number) => {
 		// live, < 2 --- dead
 		// live, 2 || 3 --- alive
 		// live , > 3 --- dead
 		// dead, === 3 --- alive
-	  
+
 		const aliveNeighborsCount = countNeighbors(row, col, 2);
-	  
+
 		if (ownState === 0) {
-		  if (aliveNeighborsCount === 3) return 1;
-		  else return 0;
+			if (aliveNeighborsCount === 3) return 1;
+			else return 0;
 		} else {
-		  if (aliveNeighborsCount < 10 || 19 < aliveNeighborsCount) return 0;
-		  else return ownState + 1;
+			if (aliveNeighborsCount < 10 || 19 < aliveNeighborsCount) return 0;
+			else return ownState + 1;
 		}
-	  };
-	  
-	  const countNeighbors = (row:number, col:number, radius:number) => {
+	};
+
+	const countNeighbors = (row: number, col: number, radius: number) => {
 		let aliveNeighborsCount = 0;
-	  
+
 		for (let r = -radius; r <= radius; r++) {
-		  for (let c = -radius; c <= radius; c++) {
-			if (r === 0 && c === 0) continue;
-			const neighbor = activeCells[mod(row + r, ROWS)][mod(col + c, COLS)];
-			if (neighbor > 0) aliveNeighborsCount++;
-		  }
+			for (let c = -radius; c <= radius; c++) {
+				if (r === 0 && c === 0) continue;
+				const neighbor = activeCells[mod(row + r, ROWS)][mod(col + c, COLS)];
+				if (neighbor > 0) aliveNeighborsCount++;
+			}
 		}
-	  
+
 		return aliveNeighborsCount;
-	  };
-	  
-	  // --------- RENDER
-	  
-	  const renderCells = () => {
+	};
+
+	// --------- RENDER
+
+	const renderCells = () => {
 		p.noStroke();
 		for (let r = 0; r < ROWS; r++) {
-		  for (let c = 0; c < COLS; c++) {
-			const state = activeCells[r][c];
-			if (state === 0) continue;
-			p.fill(100, 0, p.max(50, 200 - getColorForState(state)));
-			p.square(c * SIZE, r * SIZE, SIZE);
-		  }
+			for (let c = 0; c < COLS; c++) {
+				const state = activeCells[r][c];
+				if (state === 0) continue;
+				p.fill(100, 0, p.max(50, 200 - getColorForState(state)));
+				p.square(c * SIZE, r * SIZE, SIZE);
+			}
 		}
-	  };
-	  
-	  const getColorForState = (state: number) => {
+	};
+
+	const getColorForState = (state: number) => {
 		return state * 5;
-	  };
-	  
-	  const drawGrid = () => {
+	};
+
+	const drawGrid = () => {
 		p.strokeWeight(1);
 		p.stroke(40);
 		for (let r = 0; r < ROWS; r++) p.line(0, r * SIZE, WIDTH, r * SIZE);
 		for (let c = 0; c < COLS; c++) p.line(c * SIZE, 0, c * SIZE, HEIGHT);
-	  };
-	  
-	  // ------ UTILS
+	};
+
+	// ------ UTILS
 	const mod = (n: number, m: number) => ((n % m) + m) % m;
 
 });
